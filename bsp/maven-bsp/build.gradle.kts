b70fd81012167451d62d4737846b9fc93b1a2aae
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2026-01-10 14:46:04 -0800

    fix(bsp): correct maven publishing coordinates to albertocavalcante/gvy

--- bsp/maven-bsp/build.gradle.kts
+++ bsp/maven-bsp/build.gradle.kts
@@ -79,7 +79,7 @@ publishing {
             pom {
                 name.set("Maven BSP Server")
                 description.set(project.description)
-                url.set("https://github.com/GroovyLanguageServer/groovy-language-server")
+                url.set("https://github.com/albertocavalcante/gvy")
 
                 licenses {
                     license {
@@ -90,15 +90,15 @@ publishing {
 
                 developers {
                     developer {
-                        id.set("groovylsp")
-                        name.set("Groovy Language Server Contributors")
+                        id.set("albertocavalcante")
+                        name.set("Alberto Cavalcante")
                     }
                 }
 
                 scm {
-                    connection.set("scm:git:git://github.com/GroovyLanguageServer/groovy-language-server.git")
-                    developerConnection.set("scm:git:ssh://github.com/GroovyLanguageServer/groovy-language-server.git")
-                    url.set("https://github.com/GroovyLanguageServer/groovy-language-server")
+                    connection.set("scm:git:git://github.com/albertocavalcante/gvy.git")
+                    developerConnection.set("scm:git:ssh://github.com/albertocavalcante/gvy.git")
+                    url.set("https://github.com/albertocavalcante/gvy")
                 }
             }
         }
